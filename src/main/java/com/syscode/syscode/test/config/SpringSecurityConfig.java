fcb4ac160463cb4e4b857a92c6478fcad11ee784
Kristóf Nagy <kristof.nagy@commitsmart.com>
2023-09-23 08:38:46 +0200

    fix: remove deprecated config

--- src/main/java/com/syscode/syscode/test/config/SpringSecurityConfig.java
+++ src/main/java/com/syscode/syscode/test/config/SpringSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.syscode.syscode.test.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,29 +16,37 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 
 @Configuration
 public class SpringSecurityConfig {
-  @Bean
-  public InMemoryUserDetailsManager userDetailsManager() {
-    UserDetails user =
-        User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("admin")
-            .roles("ADMIN")
-            .build();
-    return new InMemoryUserDetailsManager(user);
-  }
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable();
-    http.cors();
-    http.authorizeHttpRequests(
-            (authorize) ->
-                authorize
-                    .requestMatchers(antMatcher("/api/v1/address"))
-                    .hasRole("ADMIN")
-                    .anyRequest()
-                    .permitAll())
-        .httpBasic(Customizer.withDefaults());
-    return http.build();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.builder()
+                .passwordEncoder(passwordEncoder::encode)
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+        http.authorizeHttpRequests(
+                        (authorize) ->
+                                authorize
+                                        .requestMatchers(antMatcher("/api/v1/address"))
+                                        .hasRole("ADMIN")
+                                        .anyRequest()
+                                        .permitAll())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
